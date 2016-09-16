5f9f4c482af45723a126abf87b8170331aae6466
Julie DeMasters <juliekdemasters@gmail.com>
2016-09-16 16:43:20 -0600

    cleaned up code, removed some comments

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,203 +3,187 @@ app.controller('meanTeacontroller', function($scope, teaFactory) {
   $scope.view = {};
   $scope.quantities = [1,2,3,4,5,6,7,8,9,10];
 
-$scope.addItem = function (tea, qty) {
-  var cart = teaFactory.currentItems;
-  tea.qty = qty;
-//   //**************************************************
-  if(cart.length == 0){
-    cart.push(tea);
-  } else {
-  for (var j = 0; j < cart.length; j++) {
-    console.log("cart[j]");
-    console.log(cart[j]);
-    console.log("hey");
-    if (cart.indexOf(tea) === -1) {
-        cart.push(tea);
-        break;
+  $scope.addItem = function (tea, qty) {
+    var cart = teaFactory.currentItems;
+    tea.qty = qty;
+    if(cart.length == 0){
+      cart.push(tea);
     } else {
-      cart.tea.qty += qty
-      // break;
-      }
+      for (var j = 0; j < cart.length; j++) {
+        console.log("cart[j]");
+        console.log(cart[j]);
+        console.log("hey");
+        if (cart.indexOf(tea) === -1) {
+            cart.push(tea);
+            break;
+        }else {
+          cart.tea.qty += qty
+          }
+        }
     }
-  }
-  tea.qty = qty
-  // teaFactory.currentItems.push(tea);
-  teaQuantity = tea.qty
-}
-//************************************above is pushing twice
-//
-// console.log("cart");
-// console.log(teaFactory.currentItems);
-// //**************************************************
-//   for (var j = 0; j < cart.length; j++) {
-//     for (var i = 0; i < tea.length; i++) {
-//     if (tea[i].name == cart[j].name) {
-//       cart[j].tea.qty += Number(qty)
-//     } else {
-//       cart[j].tea.qty = Number(qty);
-//       }
-//     }
-// }
-//
-// check to see if there’s an index of
-// and if there isn’t push into array
-// else update qty
+    tea.qty = qty
+    teaQuantity = tea.qty
+  };
 
-$scope.numItemsInCart = function(){
-  for (var i = 0; i < teaQuantity.length; i++) {
-    console.log(teaQuantity[i]);
-  // if(teaQuantity[i] >1){
-    $scope.numItemsInCart += teaQuantity[i]
-    // } else{
-    //   $scope.numItemsInCart = teaQuantity
-    // }
-    // $scope.numItemsInCart = teaFactory.currentItems[0].qty
-  }
-  $scope.numItemsInCart = teaFactory.currentItems[0].qty
-}
+  //number of items in cart(updates button quantity)
+  $scope.numItemsInCart = function(){
+    for (var i = 0; i < teaQuantity.length; i++) {
+      console.log(teaQuantity[i]);
+    // if(teaQuantity[i] >1){
+      $scope.numItemsInCart += teaQuantity[i]
+      // } else{
+      //   $scope.numItemsInCart = teaQuantity
+      // }
+      // $scope.numItemsInCart = teaFactory.currentItems[0].qty
+    }
+    $scope.numItemsInCart = teaFactory.currentItems[0].qty
+  };
 
 
-$scope.view.availableItems = [ //need this information here
-  {
-    "_id": "55c8ee82152165d244b98300",
-    "name": "Bayard stew",
-    "ingredients": "concentrated gluten, jewelry, dill, beetle nut, toast",
-    "caffeineScale": 244,
-    "price": 1540,
-    "inStock": true,
-    "rating": 1,
-    "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32664_d?$cimg$",
-    "__v": 0,
-    "categories": [ "dark", "cold"]
-  },
+  $scope.view.availableItems = [ //need this information here
+    {
+      "_id": "55c8ee82152165d244b98300",
+      "name": "Bayard stew",
+      "ingredients": "concentrated gluten, jewelry, dill, beetle nut, toast",
+      "caffeineScale": 244,
+      "price": 1540,
+      "inStock": true,
+      "rating": 1,
+      "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32664_d?$cimg$",
+      "__v": 0,
+      "categories": [ "dark", "cold"]
+    },
 
-  {
-    "_id": "55c8ee82152165d244b98301",
-    "name": "Incompactness syrup",
-    "ingredients": "fennel, nutmeg leaves, parsley, cream of 'cream of cream', blarney",
-    "caffeineScale": 49,
-    "price": 7348,
-    "inStock": true,
-    "rating": 2,
-    "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32303_d?$cimg$",
-    "__v": 0,
-    "categories": ["awesome"]
-  },
-  {
-    "_id": "55c8ee82152165d244b98302",
-    "name": "Flexner white tea",
-    "ingredients": "hot sauce, iron, beetle nut, fresco, blarney, raw mashed potato",
-    "caffeineScale": 38,
-    "price": 4991,
-    "inStock": true,
-    "rating": 4,
-    "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31358_d?$cimg$",
-    "__v": 0,
-    "categories": ["cold"]
-  },
-  {
-    "_id": "55c8ee82152165d244b98303",
-    "name": "Pressor leaf",
-    "ingredients": "purina chow, flavorings, pepper, acorns, quality tallow, old sock, bay leaf",
-    "caffeineScale": 153,
-    "price": 5496,
-    "inStock": true,
-    "rating": 1,
-    "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31358_d?$cimg$",
-    "__v": 0,
-    "categories": ["dry", "hot", "awesome"]
-  },
-  {
-    "_id": "55c8ee82152165d244b98304",
-    "name": "Flexner veggie tea",
-    "ingredients": "cream of tartar, eggplant, cake, deer antler",
-    "caffeineScale": 181,
-    "price": 2445,
-    "inStock": true,
-    "rating": 1,
-    "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32621_d?$cimg$",
-    "__v": 0,
-    "categories": ["summer"]
-  },
-  {
-    "_id": "55c8ee82152165d244b98305",
-    "name": "Topflighter malt",
-    "ingredients": "botox, toast, cream of 'cream of 'cream of cream'', kitchen scraps, beef, aligator tongue, lawn clippings",
-    "caffeineScale": 241,
-    "price": 4486,
-    "inStock": true,
-    "rating": 3,
-    "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31359_d?$cimg$",
-    "__v": 0,
-    "categories": ["dry","lucid","warm"]
-  },
-  {
-    "_id": "55c8ee82152165d244b98306",
-    "name": "Cooking mix",
-    "ingredients": "flavorings, roasted mushrooms, toast, tumeric",
-    "caffeineScale": 230,
-    "price": 6973,
-    "inStock": true,
-    "rating": 3,
-    "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32303_d?$cimg$",
-    "__v": 0,
-    "categories": ["summer"]
-  },
-  {
-    "_id": "55c8ee82152165d244b98307",
-    "name": "Cooking stew",
-    "ingredients": "eggplant",
-    "caffeineScale": 122,
-    "price": 6003,
-    "inStock": true,
-    "rating": 2,
-    "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31358_d?$cimg$",
-    "__v": 0,
-    "categories": ["dry","winter","lucid"]
-  },
-  {
-    "_id": "55c8ee82152165d244b98308",
-    "name": "Prevenient herb tea",
-    "ingredients": "cream of tartar, cream of cream, kitchen scraps, flavorings",
-    "caffeineScale": 196,
-    "price": 1374,
-    "inStock": true,
-    "rating": 3,
-    "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32174_d?$cimg$",
-    "__v": 0,
-    "categories": ["lucid","hot"]
-  },
-  {
-    "_id": "55c8ee82152165d244b98309",
-    "name": "Angular mix",
-    "ingredients": "hot sauce, lawn clippings, fennel, parsley, quinine",
-    "caffeineScale": 196,
-    "price": 4158,
-    "inStock": true,
-    "rating": 2,
-    "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32621_d?$cimg$",
-    "__v": 0,
-    "categories": ["spring", "warm","winter"]
-  }
-]
+    {
+      "_id": "55c8ee82152165d244b98301",
+      "name": "Incompactness syrup",
+      "ingredients": "fennel, nutmeg leaves, parsley, cream of 'cream of cream', blarney",
+      "caffeineScale": 49,
+      "price": 7348,
+      "inStock": true,
+      "rating": 2,
+      "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32303_d?$cimg$",
+      "__v": 0,
+      "categories": ["awesome"]
+    },
+    {
+      "_id": "55c8ee82152165d244b98302",
+      "name": "Flexner white tea",
+      "ingredients": "hot sauce, iron, beetle nut, fresco, blarney, raw mashed potato",
+      "caffeineScale": 38,
+      "price": 4991,
+      "inStock": true,
+      "rating": 4,
+      "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31358_d?$cimg$",
+      "__v": 0,
+      "categories": ["cold"]
+    },
+    {
+      "_id": "55c8ee82152165d244b98303",
+      "name": "Pressor leaf",
+      "ingredients": "purina chow, flavorings, pepper, acorns, quality tallow, old sock, bay leaf",
+      "caffeineScale": 153,
+      "price": 5496,
+      "inStock": true,
+      "rating": 1,
+      "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31358_d?$cimg$",
+      "__v": 0,
+      "categories": ["dry", "hot", "awesome"]
+    },
+    {
+      "_id": "55c8ee82152165d244b98304",
+      "name": "Flexner veggie tea",
+      "ingredients": "cream of tartar, eggplant, cake, deer antler",
+      "caffeineScale": 181,
+      "price": 2445,
+      "inStock": true,
+      "rating": 1,
+      "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32621_d?$cimg$",
+      "__v": 0,
+      "categories": ["summer"]
+    },
+    {
+      "_id": "55c8ee82152165d244b98305",
+      "name": "Topflighter malt",
+      "ingredients": "botox, toast, cream of 'cream of 'cream of cream'', kitchen scraps, beef, aligator tongue, lawn clippings",
+      "caffeineScale": 241,
+      "price": 4486,
+      "inStock": true,
+      "rating": 3,
+      "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31359_d?$cimg$",
+      "__v": 0,
+      "categories": ["dry","lucid","warm"]
+    },
+    {
+      "_id": "55c8ee82152165d244b98306",
+      "name": "Cooking mix",
+      "ingredients": "flavorings, roasted mushrooms, toast, tumeric",
+      "caffeineScale": 230,
+      "price": 6973,
+      "inStock": true,
+      "rating": 3,
+      "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32303_d?$cimg$",
+      "__v": 0,
+      "categories": ["summer"]
+    },
+    {
+      "_id": "55c8ee82152165d244b98307",
+      "name": "Cooking stew",
+      "ingredients": "eggplant",
+      "caffeineScale": 122,
+      "price": 6003,
+      "inStock": true,
+      "rating": 2,
+      "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31358_d?$cimg$",
+      "__v": 0,
+      "categories": ["dry","winter","lucid"]
+    },
+    {
+      "_id": "55c8ee82152165d244b98308",
+      "name": "Prevenient herb tea",
+      "ingredients": "cream of tartar, cream of cream, kitchen scraps, flavorings",
+      "caffeineScale": 196,
+      "price": 1374,
+      "inStock": true,
+      "rating": 3,
+      "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32174_d?$cimg$",
+      "__v": 0,
+      "categories": ["lucid","hot"]
+    },
+    {
+      "_id": "55c8ee82152165d244b98309",
+      "name": "Angular mix",
+      "ingredients": "hot sauce, lawn clippings, fennel, parsley, quinine",
+      "caffeineScale": 196,
+      "price": 4158,
+      "inStock": true,
+      "rating": 2,
+      "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32621_d?$cimg$",
+      "__v": 0,
+      "categories": ["spring", "warm","winter"]
+    }
+  ]
 })
 
 //****************************************************************************
+//****************************************************************************
+//****************************************************************************
 
 app.controller('shoppingCart', function($scope, teaFactory){
-  $scope.view = {}
-  $scope.view.teaInCart = teaFactory.cartShopping()
+  $scope.view = {};
+  $scope.view.teaInCart = teaFactory.cartShopping();
   console.log('tea: ',teaFactory.currentItems);
+  console.log('tea qty: ',teaFactory.currentItems[0].qty);
+
 
 //update qty function
   function updateQty (){
-    var cart = teaFactory.currentItems
+    var cart = teaFactory.currentItems;
     for (var i = 0; i < cart.length; i++) {
-    var qty = cart[i].qty
-    $scope.qty = cart[i].qty
+    var qty = cart[i].qty;
+    $scope.qty = cart[i].qty;
     }
-  }
+  };
 
 
   function getTotal (){
@@ -210,24 +194,24 @@ app.controller('shoppingCart', function($scope, teaFactory){
       console.log($scope.sum);
     }
   }
-  getTotal()
+  getTotal();
 
   function updatePrice () {
     $scope.sum = getTotal();
     for(var i = 0; i < teaFactory.currentItems.length; i++) {
       $scope.sum = (teaFactory.currentItems[i].price);
     }
-  }
+  };
 
   $scope.removeItem = function($index){
     $scope.view.teaInCart.splice($index,1);
-    updatePrice($index)
-  }
+    updatePrice($index);
+  };
 
   $scope.edit = function($index, data) {
-    var teas = $scope.view.tea
+    var teas = $scope.view.tea;
     $scope.teaInCart[$index] = data;
-    updatePrice ()
-    updateQty ()
+    updatePrice ();
+    updateQty ();
   };
 })
--- app/js/services.js
+++ app/js/services.js
@@ -1,192 +1,148 @@
 app.factory('teaFactory', function(){
 
-return {
-  currentItems: [],
+  return {
+    currentItems: [],
+    getTotal: function (){
+      var cart = teaFactory.currentItems;
+      var sum = 0;
+      var qty = 0;
+      for (var i = 0; i < cart.length; i++) {
+        sum += (cart[i].price);
+        qty += cart[i].qty
+        console.log("here is the cart quantities");
+        console.log(qty += cart[i].qty);
+      }
+      var total = (sum * qty)
+      console.log(total);
+      return total
+      $scope.totalCost = total;
+      $scope.totalCost = ($scope.totalCost/100);
+    },
+    cartShopping: function(){
+      return this.currentItems
+    },
+     all_teas: [
+      {
+        "_id": "55c8ee82152165d244b98300",
+        "name": "Bayard stew",
+        "ingredients": "concentrated gluten, jewelry, dill, beetle nut, toast",
+        "caffeineScale": 244,
+        "price": 1540,
+        "inStock": true,
+        "rating": 1,
+        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32664_d?$cimg$",
+        "__v": 0,
+        "categories": [ "dark", "cold"]
+      },
 
-//   updatePrice: function () {
-//     var sum = 0;
-//     for(var i = 0; i < teaFactory.currentItems.length; i++) {
-//       sum += (teaFactory.currentItems[i].price);
-//     }
-getTotal: function (){
-  var cart = teaFactory.currentItems;
-  var sum = 0;
-  var qty = 0;
-  // teaCart = teaFactory.currentItems;
-  for (var i = 0; i < cart.length; i++) {
-    sum += (cart[i].price);
-    qty += cart[i].qty
-console.log("here is the cart quantities");
-console.log(qty += cart[i].qty);
-}
-// $scope.getTotal = Math.floor($scope.getTotal
-var total = (sum * qty)
-console.log(total);
-return total
-$scope.totalCost = total;
-$scope.totalCost = ($scope.totalCost/100);
-  },
-//     // console.log($scope.totalCost* tea.qty);
-//   },
-cartShopping: function(){
-  return this.currentItems
-},
-// addItem: function (tea, qty) {
-//   var cart = currentItems;
-//   // iterate over currentItems and see if tea exists
-//   // if it does, cart[i].qty += qty ;
-//   // else
-//   for (var j = 0; j < cart.length; j++) {
-//     if (tea.name == cart.name) {
-//       cart[j].tea.qty += qty
-//     } else {
-//       cart[j].tea.qty = 1;
-//       }
-// }
-// tea.qty = qty
-// currentItems.push(tea);
-// teaQuantity = tea.qty
-// },
-//
-// numItemsInCart: function(){
-//   numItemsInCart = 0;
-//     for (var i = 0; i < tea.length; i++) {
-//       console.log(teaQuantity[i]);
-//     if(teaQuantity[i] >1){
-//       numItemsInCart += teaQuantity[i]
-//     } else{
-//     numItemsInCart = 1
-//     }
-//     return numItemsInCart
-//     console.log("WHAT IS teaQuantity");
-//     console.log(teaQuantity);
-//   }
-// //    //this is how it was to track the num in cart
-// //   teaFactory.updatePrice()
-// },
-   all_teas: [
-    {
-      "_id": "55c8ee82152165d244b98300",
-      "name": "Bayard stew",
-      "ingredients": "concentrated gluten, jewelry, dill, beetle nut, toast",
-      "caffeineScale": 244,
-      "price": 1540,
-      "inStock": true,
-      "rating": 1,
-      "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32664_d?$cimg$",
-      "__v": 0,
-      "categories": [ "dark", "cold"]
-    },
-
-    {
-      "_id": "55c8ee82152165d244b98301",
-      "name": "Incompactness syrup",
-      "ingredients": "fennel, nutmeg leaves, parsley, cream of 'cream of cream', blarney",
-      "caffeineScale": 49,
-      "price": 7348,
-      "inStock": true,
-      "rating": 2,
-      "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32303_d?$cimg$",
-      "__v": 0,
-      "categories": ["awesome"]
-    },
-    {
-      "_id": "55c8ee82152165d244b98302",
-      "name": "Flexner white tea",
-      "ingredients": "hot sauce, iron, beetle nut, fresco, blarney, raw mashed potato",
-      "caffeineScale": 38,
-      "price": 4991,
-      "inStock": true,
-      "rating": 4,
-      "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31358_d?$cimg$",
-      "__v": 0,
-      "categories": ["cold"]
-    },
-    {
-      "_id": "55c8ee82152165d244b98303",
-      "name": "Pressor leaf",
-      "ingredients": "purina chow, flavorings, pepper, acorns, quality tallow, old sock, bay leaf",
-      "caffeineScale": 153,
-      "price": 5496,
-      "inStock": true,
-      "rating": 1,
-      "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31358_d?$cimg$",
-      "__v": 0,
-      "categories": ["dry", "hot", "awesome"]
-    },
-    {
-      "_id": "55c8ee82152165d244b98304",
-      "name": "Flexner veggie tea",
-      "ingredients": "cream of tartar, eggplant, cake, deer antler",
-      "caffeineScale": 181,
-      "price": 2445,
-      "inStock": true,
-      "rating": 1,
-      "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32621_d?$cimg$",
-      "__v": 0,
-      "categories": ["summer"]
-    },
-    {
-      "_id": "55c8ee82152165d244b98305",
-      "name": "Topflighter malt",
-      "ingredients": "botox, toast, cream of 'cream of 'cream of cream'', kitchen scraps, beef, aligator tongue, lawn clippings",
-      "caffeineScale": 241,
-      "price": 4486,
-      "inStock": true,
-      "rating": 3,
-      "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31359_d?$cimg$",
-      "__v": 0,
-      "categories": ["dry","lucid","warm"]
-    },
-    {
-      "_id": "55c8ee82152165d244b98306",
-      "name": "Cooking mix",
-      "ingredients": "flavorings, roasted mushrooms, toast, tumeric",
-      "caffeineScale": 230,
-      "price": 6973,
-      "inStock": true,
-      "rating": 3,
-      "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32303_d?$cimg$",
-      "__v": 0,
-      "categories": ["summer"]
-    },
-    {
-      "_id": "55c8ee82152165d244b98307",
-      "name": "Cooking stew",
-      "ingredients": "eggplant",
-      "caffeineScale": 122,
-      "price": 6003,
-      "inStock": true,
-      "rating": 2,
-      "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31358_d?$cimg$",
-      "__v": 0,
-      "categories": ["dry","winter","lucid"]
-    },
-    {
-      "_id": "55c8ee82152165d244b98308",
-      "name": "Prevenient herb tea",
-      "ingredients": "cream of tartar, cream of cream, kitchen scraps, flavorings",
-      "caffeineScale": 196,
-      "price": 1374,
-      "inStock": true,
-      "rating": 3,
-      "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32174_d?$cimg$",
-      "__v": 0,
-      "categories": ["lucid","hot"]
-    },
-    {
-      "_id": "55c8ee82152165d244b98309",
-      "name": "Angular mix",
-      "ingredients": "hot sauce, lawn clippings, fennel, parsley, quinine",
-      "caffeineScale": 196,
-      "price": 4158,
-      "inStock": true,
-      "rating": 2,
-      "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32621_d?$cimg$",
-      "__v": 0,
-      "categories": ["spring", "warm","winter"]
-    }
-  ]
+      {
+        "_id": "55c8ee82152165d244b98301",
+        "name": "Incompactness syrup",
+        "ingredients": "fennel, nutmeg leaves, parsley, cream of 'cream of cream', blarney",
+        "caffeineScale": 49,
+        "price": 7348,
+        "inStock": true,
+        "rating": 2,
+        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32303_d?$cimg$",
+        "__v": 0,
+        "categories": ["awesome"]
+      },
+      {
+        "_id": "55c8ee82152165d244b98302",
+        "name": "Flexner white tea",
+        "ingredients": "hot sauce, iron, beetle nut, fresco, blarney, raw mashed potato",
+        "caffeineScale": 38,
+        "price": 4991,
+        "inStock": true,
+        "rating": 4,
+        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31358_d?$cimg$",
+        "__v": 0,
+        "categories": ["cold"]
+      },
+      {
+        "_id": "55c8ee82152165d244b98303",
+        "name": "Pressor leaf",
+        "ingredients": "purina chow, flavorings, pepper, acorns, quality tallow, old sock, bay leaf",
+        "caffeineScale": 153,
+        "price": 5496,
+        "inStock": true,
+        "rating": 1,
+        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31358_d?$cimg$",
+        "__v": 0,
+        "categories": ["dry", "hot", "awesome"]
+      },
+      {
+        "_id": "55c8ee82152165d244b98304",
+        "name": "Flexner veggie tea",
+        "ingredients": "cream of tartar, eggplant, cake, deer antler",
+        "caffeineScale": 181,
+        "price": 2445,
+        "inStock": true,
+        "rating": 1,
+        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32621_d?$cimg$",
+        "__v": 0,
+        "categories": ["summer"]
+      },
+      {
+        "_id": "55c8ee82152165d244b98305",
+        "name": "Topflighter malt",
+        "ingredients": "botox, toast, cream of 'cream of 'cream of cream'', kitchen scraps, beef, aligator tongue, lawn clippings",
+        "caffeineScale": 241,
+        "price": 4486,
+        "inStock": true,
+        "rating": 3,
+        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31359_d?$cimg$",
+        "__v": 0,
+        "categories": ["dry","lucid","warm"]
+      },
+      {
+        "_id": "55c8ee82152165d244b98306",
+        "name": "Cooking mix",
+        "ingredients": "flavorings, roasted mushrooms, toast, tumeric",
+        "caffeineScale": 230,
+        "price": 6973,
+        "inStock": true,
+        "rating": 3,
+        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32303_d?$cimg$",
+        "__v": 0,
+        "categories": ["summer"]
+      },
+      {
+        "_id": "55c8ee82152165d244b98307",
+        "name": "Cooking stew",
+        "ingredients": "eggplant",
+        "caffeineScale": 122,
+        "price": 6003,
+        "inStock": true,
+        "rating": 2,
+        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31358_d?$cimg$",
+        "__v": 0,
+        "categories": ["dry","winter","lucid"]
+      },
+      {
+        "_id": "55c8ee82152165d244b98308",
+        "name": "Prevenient herb tea",
+        "ingredients": "cream of tartar, cream of cream, kitchen scraps, flavorings",
+        "caffeineScale": 196,
+        "price": 1374,
+        "inStock": true,
+        "rating": 3,
+        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32174_d?$cimg$",
+        "__v": 0,
+        "categories": ["lucid","hot"]
+      },
+      {
+        "_id": "55c8ee82152165d244b98309",
+        "name": "Angular mix",
+        "ingredients": "hot sauce, lawn clippings, fennel, parsley, quinine",
+        "caffeineScale": 196,
+        "price": 4158,
+        "inStock": true,
+        "rating": 2,
+        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32621_d?$cimg$",
+        "__v": 0,
+        "categories": ["spring", "warm","winter"]
+      }
+    ]
   };
 })
-// })
