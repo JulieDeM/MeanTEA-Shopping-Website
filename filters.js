774ebe9786d29cf85b9325eeed1618d623aab635
Julie DeMasters <juliekdemasters@gmail.com>
2017-02-17 19:21:38 -0700

    removed files

--- filters.js
+++
@@ -1,6 +0,0 @@
-app.filter('categoryFilter', function () {
-  return  sortableFields = ['dark', 'cold', 'awesome', 'dry', 'hot', 'summer', 'lucid', 'warm', 'winter', 'spring'];
-});
-// $scope.view.sortableFields = ['dark', 'cold', 'awesome', 'dry', 'hot', 'summer', 'lucid', 'warm', 'winter', 'spring'];
-// $scope.categoriesarray = categoriesarray.filter
-// $scope.sortcats = 'categories'
